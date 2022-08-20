186ea0853cc67ee804fa81b2de36b40427d5f711
Michael Spencer <32441294+michael-spencer31@users.noreply.github.com>
2022-08-20 10:15:29 -0300

    Updated
    Cleaned up some things and got the ability api call working.

--- poke/assests/javascript/javascript.js
+++ poke/assests/javascript/javascript.js
@@ -1,68 +1,55 @@
-var teamListElement = $("#team-list");
-var teamElement = $("#team");
-var playerListElement = $("#player-list");
-var playerElement = $("#player");
-
-// create a list of all team IDs for later
-var teamIDList = [];
-var playerIDList = [];
-
-function empty(){
-    //document.getElementById("imghold").remove();
-
-    //doucment.getElementById("demo").remove();
+unction empty(){
+    doucment.getElementById("demo").remove();
 }
 
-function displayinput(){
+function getAbilities(){
+
     var input = document.getElementById("demo").innerHTML = document.getElementById("userin").value;
     var abilityURL = "https://pokeapi.co/api/v2/ability/" + input + "/";
-    var pokemonURL = "https://pokeapi.co/api/v2/pokemon/mudkip/";
 
     $.ajax({
         url: abilityURL,
         method: "GET"
     }).done(function(data){
-        
+       
+        document.getElementById('demo').innerHTML = data.effect_entries[1].effect;
 
-        //document.getElementById("placehere").appendChild(elem);
+        var names = [];
 
-        /**
-        imgSource = data.sprites.front_default;
-        var x = new Image();
-        x.src = imgSource;
-        document.getElementById("imghold").appendChild(x);
-        */
-        //document.getElementById("demo").innerHTML = data.sprites.front_default;
-        //document.getElementById("demo").innerHTML = data.effect_entries[1].effect;
-        //document.getElementById("imghold").innerHTML = data.pokemon[0].pokemon.name;
-
-        
-        //var names = [];
-        
-        for(let i = 0; i < 3; i++){
-            //document.getElementById("demo").appendChild(data.pokemon[i].pokemon.name);
-            //names[i] = data.pokemon[i].pokemon.name;
+        var pokemonNumber = data['pokemon'].length      
+        for(let i = 0; i < pokemonNumber; i++){
+            names[i] = data.pokemon[i].pokemon.name;
             let ptag = document.createElement('p');
             ptag.classList.add(data.pokemon[i].pokemon.name);
             ptag.id = i;
             ptag.innerHTML = data.pokemon[i].pokemon.name;
-            document.getElementById('demo').appendChild(ptag);
         }
-        document.getElementById("demo").appendChild("emma");
-        $.ajax({
-            url: pokemonURL,
-            method: "GET"
-        }).done(function(pokeData){
+       
+        var pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + names[0] + "/";
+        var counter = 1;
 
-            document.getElementById("demo").innerHTML = "emma";
+        for(let i = 0; i < pokemonNumber; i++){
 
+            $.ajax({
+                url: pokemonURL,
+                method: "GET",
+                statusCode: {
+                    404: function(responseObject, textStatus, jqXHR){
 
-        });
+                    },
+                    200: function(responseObject, textStatus, jqXHR){
+                       
+                    }
+                }
+            }).done(function(pokeData){
+
+                imgSource = pokeData.sprites.front_default;
+                var x = new Image();
+                x.src = imgSource;
+                document.getElementById('test').appendChild(x);            
+            });
+            pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + names[counter] + "/";
+            counter++;
+        }
     });
 }
-
-
-
-
-
-
