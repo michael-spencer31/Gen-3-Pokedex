0ab5c967679827d166a2cccc02302b3e2099c688
Michael Spencer <32441294+michael-spencer31@users.noreply.github.com>
2022-08-21 22:43:25 -0300

    Update javascript.js
    Added code to reflect new changes in HTML file.

--- poke/assests/javascript/javascript.js
+++ poke/assests/javascript/javascript.js
@@ -1,5 +1,6 @@
 function empty(){
     doucment.getElementById("abilityDescription").remove();
+    //doucment.getElementById("pokemontype").remove();
 }
 
 /* this code handle all ability related activities */
@@ -58,17 +59,57 @@ function getAbilities(){
 }
 function getPokemon(){
 
-    var pokeURL = "https://pokeapi.co/api/v2/pokemon/crobat";
+    var input = document.getElementById('pokemonin').value;
+    var pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + input + "/";
 
     $.ajax({
-        url: pokeURL,
+        url: pokemonURL,
         method: "GET"
     }).done(function(pokemonData){
 
-        document.getElementById('pokemondata').innerHTML = pokemonData.height * 10 + "cm\n";
-        document.getElementById('pokemondata').append("\n\n\n");
-        document.getElementById('pokemondata').append(pokemonData.types[0].type.name + " ");
-        document.getElementById('pokemondata').append(pokemonData.types[1].type.name);
+        imgSource = pokemonData.sprites.front_default;
+        var z = new Image();
+        z.src = imgSource;
+        document.getElementById('pokemonimage').appendChild(z);
+
+        document.getElementById('height').innerHTML = pokemonData.height * 10 + "cm\n";
+        document.getElementById('weight').innerHTML = pokemonData.weight / 10 + "kg\n";
+
+        //determine if pokemon has 1 or 2 types than print them
+        var numberOfTypes = (pokemonData.types).length;
+        document.getElementById('pokemontype').innerHTML = (pokemonData.types[0].type.name);
+
+        //this indicates the pokemon has 2 types
+        if(numberOfTypes == 2){
+            document.getElementById('secondtype').innerHTML = ("/" + pokemonData.types[1].type.name);
+        }
+        var itemLength = (pokemonData.held_items).length;
+
+        if(itemLength != 0){
+            document.getElementById('helditemdata').innerHTML = (pokemonData.held_items[0].item.name);
+        }else{
+            document.getElementById('helditemdata').innerHTML = ("None");
+        }
+        document.getElementById('pokemonability').innerHTML = pokemonData.abilities[0].ability.name;
+
+        var numberOfAbilities = (pokemonData.abilities).length;
+
+        //check if a pokemon has a second ability
+        if(numberOfAbilities == 2){
+            document.getElementById('secondability').innerHTML = pokemonData.abilities[1].ability.name;
+        }else{
+            document.getElementById('secondability').innerHTML = "None";
+        }
+        //check if a pokemon has a second or hidden ability
+        if(numberOfAbilities == 3){
+            document.getElementById('secondability').innerHTML = pokemonData.abilities[1].ability.name;
+            document.getElementById('hiddenability').innerHTML = pokemonData.abilities[2].ability.name;
+        }else{
+            document.getElementById('hiddenability').innerHTML = "None";
+        }
+        document.getElementById('pokedexnumber').innerHTML = pokemonData.game_indices[0].game_index;
+        //document.getElementById('pokemonimage').removeChild(z);
+
     });
 }
 function getItems(){
@@ -89,6 +130,24 @@ function getItems(){
         var y = new Image();
         y.src = itemImg;
         document.getElementById('itemimage').appendChild(y);
+    });
+}
+function getMoves(){
+
+    var input = document.getElementById('movein').value;
+    var moveURL = "https://pokeapi.co/api/v2/move/" + input + "/";
+
+    $.ajax({
+        url: moveURL,
+        method: "GET"
+    }).done(function(moveData){
+
+        document.getElementById('movepower').innerHTML = moveData.power;
+        document.getElementById('moveaccuracy').innerHTML = moveData.accuracy;
+        document.getElementById('movepp').innerHTML = moveData.pp;
+        document.getElementById('movetype').innerHTML = moveData.damage_class.name;
+        document.getElementById('moveeffect').innerHTML = moveData.effect_entries[0].effect;
+
     });
 }
 //this code controls all the tabs at the top of the page
