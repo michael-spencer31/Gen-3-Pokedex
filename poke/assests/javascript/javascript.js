5b960d35eb7561f99dbf111a8a59b5a86cfa16ef
Michael Spencer <32441294+michael-spencer31@users.noreply.github.com>
2022-08-22 21:51:27 -0300

    Update javascript.js
    Added additional documentation
    Added new tab content

--- poke/assests/javascript/javascript.js
+++ poke/assests/javascript/javascript.js
@@ -3,7 +3,7 @@ function empty(){
     //doucment.getElementById("pokemontype").remove();
 }
 
-/* this code handle all ability related activities */
+/* this code handle all ability related code */
 function getAbilities(){
 
     /* create a query using the user input to call on the api */
@@ -13,14 +13,19 @@ function getAbilities(){
     //create and then call the api using the ajax jQuery method
     $.ajax({
         url: abilityURL,
+        //call the GET method on the data
         method: "GET"
     }).done(function(data){
        
+       //get the element from the HTML page and set the text using data from the api
         document.getElementById('abilityDescription').innerHTML = data.effect_entries[1].effect;
 
+        //create an array to use later
         var names = [];
 
-        var pokemonNumber = data['pokemon'].length      
+        var pokemonNumber = data['pokemon'].length  
+
+        //for each pokemon in the data, get their name    
         for(let i = 0; i < pokemonNumber; i++){
             names[i] = data.pokemon[i].pokemon.name;
             let ptag = document.createElement('p');
@@ -28,12 +33,14 @@ function getAbilities(){
             ptag.id = i;
             ptag.innerHTML = data.pokemon[i].pokemon.name;
         }
-       
+        //now, make another URL using the pokemon name found above
         var pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + names[0] + "/";
         var counter = 1;
 
+        //for each pokemon we found, loop
         for(let i = 0; i < pokemonNumber; i++){
 
+            //make a new call to the API for each pokemon name
             $.ajax({
                 url: pokemonURL,
                 method: "GET",
@@ -47,21 +54,26 @@ function getAbilities(){
                 }
             }).done(function(pokeData){
 
+                //create an image for each pokemon and add it to the HTML
                 imgSource = pokeData.sprites.front_default;
                 var x = new Image();
                 x.src = imgSource;
                 document.getElementById('abilityImages').appendChild(x);            
             });
+            //change the url for the call we are going to make
             pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + names[counter] + "/";
             counter++;
         }
     });
 }
+//this function gets data about the specified pokemon
 function getPokemon(){
 
+    //get the input from the html document and create a url
     var input = document.getElementById('pokemonin').value;
     var pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + input + "/";
 
+    //call the api with the ajax jQuery method and get data back
     $.ajax({
         url: pokemonURL,
         method: "GET"
@@ -83,8 +95,10 @@ function getPokemon(){
         if(numberOfTypes == 2){
             document.getElementById('secondtype').innerHTML = ("/" + pokemonData.types[1].type.name);
         }
+        //determine if the pokemon has a held item
         var itemLength = (pokemonData.held_items).length;
 
+        //check if the pokemon can have a held item when found in the wild
         if(itemLength != 0){
             document.getElementById('helditemdata').innerHTML = (pokemonData.held_items[0].item.name);
         }else{
@@ -108,15 +122,16 @@ function getPokemon(){
             document.getElementById('hiddenability').innerHTML = "None";
         }
         document.getElementById('pokedexnumber').innerHTML = pokemonData.game_indices[0].game_index;
-        //document.getElementById('pokemonimage').removeChild(z);
-
     });
 }
+//this function gets information about a specified item 
 function getItems(){
 
+    //get the input from the html document
     var input = document.getElementById('itemin').value;
     var itemURL = "https://pokeapi.co/api/v2/item/" + input + "/";
 
+    //call the api with the ajax jQuery method
     $.ajax({
         url: itemURL,
         method: "GET"
@@ -132,6 +147,7 @@ function getItems(){
         document.getElementById('itemimage').appendChild(y);
     });
 }
+//this function gets information about a specific move
 function getMoves(){
 
     var input = document.getElementById('movein').value;
@@ -147,8 +163,39 @@ function getMoves(){
         document.getElementById('movepp').innerHTML = moveData.pp;
         document.getElementById('movetype').innerHTML = moveData.damage_class.name;
         document.getElementById('moveeffect').innerHTML = moveData.effect_entries[0].effect;
+        document.getElementById('movepriority').innerHTML = moveData.priority;
+        document.getElementById('movehealing').innerHTML = moveData.meta.healing + "%";
+        document.getElementById('movecrit').innerHTML = "+" + moveData.meta.crit_rate;
 
     });
+}
+//this function gets information about the locations of a pokemon
+function getEncounters(){
+
+    var input = document.getElementById('encounterin').value;
+    var encounterURL = "https://pokeapi.co/api/v2/pokemon/" + input + "/encounters/";
+
+    $.ajax({
+        url: encounterURL,
+        method: "GET"
+    }).done(function(encounterData){
+
+        var encounterLength = encounterData.length;
+
+        for(var i = 0; i < encounterLength; i++){
+
+            document.getElementById('encounterdetails').innerHTML += "<br />" + encounterData[i].location_area.name 
+            + " Level:" + encounterData[i].version_details[0].encounter_details[0].min_level + "-" 
+            + encounterData[i].version_details[0].encounter_details[0].max_level + " "
+            + "Encounter Rate: " + encounterData[i].version_details[0].encounter_details[0].chance + "%";
+        }
+
+
+    });
+}
+//clear the previously obtained data from the field
+function resetEncounters(){
+    document.getElementById('encounterdetails').innerHTML = " ";
 }
 //this code controls all the tabs at the top of the page
 function openTabs(evt, tabName){
@@ -168,7 +215,4 @@ function openTabs(evt, tabName){
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-}
-function defaultTab(){
-    document.getElementById("defaultOpen").click();
 }
